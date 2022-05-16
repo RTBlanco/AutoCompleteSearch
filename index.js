d9c6b5fd7016dc0801955ce374381b3a8f9c8ad8
RTBlanco <ronnytoribio1@hotmail.com>
2022-05-16 00:50:59 -0400

    wrote basic js

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-document.addEventListener('domContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
+  const input = document.getElementById('search-bar');
+  const dropSearch = document.getElementById('search');
+
+  let timeout;
+  input.addEventListener('keyup', () => {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+
+    timeout = setTimeout(() => {
+      
+    }, 5000);
+  });
+});
+
+function renderData() {
   
-})
+}
+
+const Data = {
+  items: [
+    'do',
+    'this',
+    'integra',
+    'acura'
+  ]
+}
